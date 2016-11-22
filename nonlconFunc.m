cc549c5438166eb016feb768b8b00701aa122ffa
Ky Woodard <ky.a.woodard@gmail.com>
2016-11-22 11:36:35 -0500

    Updated nonlconso the constraint size scales with the number of joints

--- nonlconFunc.m
+++ nonlconFunc.m
@@ -3,20 +3,20 @@ function  [c,ceq,gradc,gradceq] = nonlconFunc(x,p)
 % constriant function of input x [n, 1] which returns
 % the c<=0, its gradient, ceq==0, and its gradient wrt x.
 % Given Parameters structure p
-
+nJ = p.nJoints;
 if p.useTorqueConstraint
-    th = reshape(x(1:p.nPoses*p.nJoints), [p.nJoints, p.nPoses]);
-    lengths = x(p.nPoses*p.nJoints+1 : end);
+    th = reshape(x(1:p.nPoses*nJ), [nJ, p.nPoses]);
+    lengths = x(p.nPoses*nJ+1 : end);
     rb = [0;0;0];
     % To do: add these if needed
-    tau = zeros(p.nPoses*p.nJoints,1);
+    tau = zeros(p.nPoses*nJ,1);
     for i = 1:p.nPoses
         g_temp = g_torqueFunc(th(:,i), lengths, rb);
-        tau(1+3*(i-1):3*i) = g_temp(1:3);
+        tau(1+nJ*(i-1):nJ*i) = g_temp(1:nJ);
         g_temp = gd_torqueFunc(th(:,i), lengths, rb);
-        dtau(1+3*(i-1):3*i) = g_temp(1:3);
+        dtau(1+nJ*(i-1):nJ*i) = g_temp(1:nJ);
     end
-    c=tau-p.jointMaxTorque*ones(p.nPoses*p.nJoints,1);
+    c=tau-p.jointMaxTorque*ones(p.nPoses*nJ,1);
     gradc =dtau;
 else
     c = [];
